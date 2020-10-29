4fbf7e2bce25f2bf945f9967b46e8a205c31cf1d
NeaX1X <not.mail.but.mail@gmail.com>
2020-10-29 20:57:53 +0200

    test

---
+++ 1.py
@@ -0,0 +1,10 @@
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+number4 = int(input())
+number5 = int(input())
+
+sum1 = number1 + number2 + number3 + number4 + nuber5
+
+print(sum1)
